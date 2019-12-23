81aedfe9725c91109cc085b897c0fe0a2292f951
Razvan0206 <razvan.iuga02@gmail.com>
2019-12-23 20:49:59 +0200

    Ceva basic, facut repede

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{   
+    public float JumpForce;
+    public Rigidbody2D rb;
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+          rb.AddForce(transform.up*JumpForce);
+          Debug.Log("osjfaehfw");
+        }
+
+
+     Debug.Log("osjfaehfw");
+        
+       
+    }
+}
