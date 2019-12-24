915378e7502186d46159b4ecb425823a51d49c04
Razvan0206 <razvan.iuga02@gmail.com>
2019-12-24 15:44:53 +0200

    cel mai naspa camera follow

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform Player;
+    public GameObject PlayerGam;
+    public float PlayerY1;
+    public float PlayerY2;
+    public Vector3 offset;
+    
+     void Update()
+    {
+       Follow();
+        
+      Vector3 playerPosition = Player.position;
+      PlayerY1 = playerPosition.y;
+  
+    }
+    
+    public void Follow()
+    {
+        Debug.Log("Following");
+        Vector3 position = transform.position ;
+      position.y = (Player.position + offset).y ;
+      
+      transform.position = position ;
+    }
+
+}
+ 
+
+
