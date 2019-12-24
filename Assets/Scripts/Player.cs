8db866d0ecf665056a7253b8dbc8875589953104
Razvan0206 <razvan.iuga02@gmail.com>
2019-12-24 22:23:34 +0200

    RestartScene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {   
@@ -53,6 +54,12 @@ public class Player : MonoBehaviour
         {
           rb.AddForce(transform.right*SidewaysForce);
           Debug.Log("right");
+        }
+        if(gameObject.transform.position.y < -10)
+        {
+            SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+
+
         }
 
 
