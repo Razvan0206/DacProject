5e43f60d87837f7217da57a319df2da63ef98dd2
Razvan0206 <razvan.iuga02@gmail.com>
2019-12-26 22:05:56 +0200

    Pause Done

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public bool time = false;
+    public Canvas PauseCavas;
+    public void Pause1()
+    {
+        time = ! time;
+        
+    }
+    public void Update()
+    {
+        if (time == true)
+        {
+            Time.timeScale = 0;
+            PauseCavas.enabled = true;
+        }
+        if (time == false)
+        {
+            Time.timeScale = 1;
+            PauseCavas.enabled = false;
+        }
+    }
+    public void Resume()
+    {
+        time = false;
+        PauseCavas.enabled = false;
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
