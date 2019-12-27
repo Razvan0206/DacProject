09a1e898825a86998a5698ce99ae04b0391abb9c
Razvan0206 <razvan.iuga02@gmail.com>
2019-12-27 19:46:45 +0200

    push 2020

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Advertisements;
+
 
 public class Restart : MonoBehaviour
 {
@@ -11,17 +11,7 @@ public class Restart : MonoBehaviour
     public Transform Player;
     public Canvas RestartCan;
     
-    public void Continue()
-    {
-        if(Advertisement.IsReady("rewardedVideo"))
-        {
-            Advertisement.Show("rewardedVideo");
-            Nava.position = new Vector3(Nava.position.x, Camera.position.y - 4, Nava.position.z);
-            Player.position = new Vector3(Camera.position.x, Camera.position.y, Player.position.z);
-            RestartCan.enabled = false;
-        }
-        
-    }
+    
     public void Restart1()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
